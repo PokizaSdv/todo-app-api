f8dfbac3718139b5ea6efd28d093d0bbb9d9aa47
Pokiza Sdv <pokizasdv@gmail.com>
2023-11-07 16:43:12 -0500

    Implement logic Sign In

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { userService } from "../services/user.service.js";
 import { catchAsync } from "../utils/catch-async.js";
 
+
 class UserController {
     signUp = catchAsync(async (req, res) => {
         const { body } = req;
@@ -17,6 +18,19 @@ class UserController {
         
         res.status(201).json({
             message: "Success"
+        });
+    });
+
+    signin = catchAsync(async (req, res) => {
+        const { body } = req;
+        const input = {
+            email: body.email,
+            password: body.password
+        };
+
+        const jwt = await userService.signin(input);
+        res.status(200).json({
+            token: jwt
         });
     });
 }
